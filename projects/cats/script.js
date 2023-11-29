66486e8190e019620b4962b3aef292d157543eb3
catkitty1234 <152427049+catkitty1234@users.noreply.github.com>
2023-11-29 21:17:29 +0200

    Add files via upload

---
+++ projects/cats/script.js
@@ -0,0 +1,25 @@
+let a;
+
+function love() {
+	a = confirm("Так ты любишь котиков?");
+	if (a == true){
+		alert('МЯУ! :3');
+	}
+	else {
+		alert(':c');
+	}
+}
+
+function cats_are_the_best() {
+	a = confirm("Котики самые классные?");
+	if (a == true){
+		alert('Правильна, МЯУ! :3');
+	}
+	else {
+		alert('я насру тебе под дверь :3');
+	}
+}
+
+function hello() {
+	alert("Кошачий привет!");
+}
